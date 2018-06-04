ace280a840c70ccd82256fea628a06998c7fa5ee
leahgwin <leahgwin@gmail.com>
2018-06-04 11:08:35 -0500

    completed second exercise of making it into a fragment

--- main.js
+++ main.js
@@ -3,41 +3,48 @@
 // In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
 let messages = document.querySelector("#messages");
 
+//creating a full document fragment for all of this
+const fragment = document.createDocumentFragment()
 
 // Create five (5) section elements, each with a class of message, and with textContent of your choosing.
 
-// Create an unordered list element
-const list = document.createElement('ul')
 
 // Create list items for list
-const listItem1 = document.createElement('li')
-listItem1.className = "message"
-listItem1.textContent = "What are we eating for dinner at Thanksgiving?"
+const item1 = document.createElement('div')
+item1.className = "message"
+item1.textContent = "Cousin 1: What are we eating for dinner at Thanksgiving?"
+fragment.appendChild(item1);
 
-const listItem2 = document.createElement('li')
-listItem2.className = "message"
-listItem2.textContent = "I hope Crazy Aunt Sue isn't coming this year."
-
-const listItem3 = document.createElement('li')
-listItem3.className = "message"
-listItem3.textContent = "All of the cousins should go grab a drink after Thanksgiving dinner, ok?"
-
-const listItem4 = document.createElement('li')
-listItem4.className = "message"
-listItem4.textContent = "Oh no. I heard Crazy Aunt Sue really IS coming."
-
-const listItem5 = document.createElement('li')
-listItem5.className = "message"
-listItem5.textContent = "UGH. She drives me crazy. I really will need that drink. lol"
+const item2 = document.createElement('div')
+item2.className = "message"
+item2.textContent = "Cousin 2: I hope Crazy Aunt Sue isn't coming this year."
+fragment.appendChild(item2);
 
 
-// Using appendChild(), attach each message as a child to the messages element.
-list.appendChild(listItem1);
-list.appendChild(listItem2);
-list.appendChild(listItem3);
-list.appendChild(listItem4);
-list.appendChild(listItem5);
+const item3 = document.createElement('div')
+item3.className = "message"
+item3.textContent = "Cousin 1: All of the cousins should go grab a drink after Thanksgiving dinner, ok?"
+fragment.appendChild(item3);
 
 
-//console logging to check if it worked
-console.log(list)
+const item4 = document.createElement('div')
+item4.className = "message"
+item4.textContent = "Cousin 3: Oh no. I heard Crazy Aunt Sue really IS coming."
+fragment.appendChild(item4);
+
+
+const item5 = document.createElement('div')
+item5.className = "message"
+item5.textContent = "Cousin 2: UGH. She drives me crazy. I really will need that drink. lol"
+fragment.appendChild(item5);
+
+
+// Update your code from the previous exercise to add the chat messages to the messages elements via a document fragment.
+
+/*
+    Now I can update my HTML document all at once, with all
+    three emperor components rendered. Otherwise, I would
+    have needed to add each one, individually - a much more
+    "expensive" operation.
+*/
+document.querySelector("#messages").appendChild(fragment);
